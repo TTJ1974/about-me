79a42cee405523a8aee78ac0fe76a3141553724a
TTJ1974 <johntucker1974@live.com>
2021-11-30 16:13:48 -0800

    index/js update

---
+++ js/app2.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('hello world');
+
+let banana;
+console.log(banana);
+
+//question one correct answer yes
+let answerOne = prompt('Am I from Washington?').toLowerCase();
+
+//Leaving this  to "spell it out"
+// Let normalizedAnswerOne = answerOne.tolowerCase();
+
+if (answerOne +++ 'yes || answerOne === 'y'){
+  //console.log('YES, I am from Washington!');
+  alert('YES, I am from Washington!');
+}
