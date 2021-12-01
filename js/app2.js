6b3488da88fa005aaf9a149c7f37e612fd79fb11
TTJ1974 <johntucker1974@live.com>
2021-11-30 17:26:04 -0800

    final push on ind/js

--- js/app2.js
+++ js/app2.js
@@ -1,17 +1,62 @@
 'use strict';
+let counter = 0;
 
-console.log('hello world');
+function answerQuestions_yes_or_no(Q_sting, userAnswer, trueAnswer) {
+    let score = 0;
+    while (!(userAnswer === 'yes') && !(userAnswer === 'no') && !(userAnswer === 'y') && !(userAnswer === 'n')) {
 
-let banana;
-console.log(banana);
+        alert("Yes or no answers only please... we cool?")
+        userAnswer = prompt(Q_sting).toLowerCase()
+             console.log(userAnswer)
+             console.log(userAnswer)
+    }
+    if (userAnswer === trueAnswer) {
+        score++;
+        console.log("function answer  " + userAnswer, score)
+        return score;
+    } else return score;
 
-//question one correct answer yes
-let answerOne = prompt('Am I from Washington?').toLowerCase();
-
-//Leaving this  to "spell it out"
-// Let normalizedAnswerOne = answerOne.tolowerCase();
-
-if (answerOne +++ 'yes || answerOne === 'y'){
-  //console.log('YES, I am from Washington!');
-  alert('YES, I am from Washington!');
 }
+let UserInput = prompt("Hello! Could you tell me your name?");
+while (!UserInput) {
+    UserInput = prompt('please provide a name !');
+}
+alert("welcome to my site " + UserInput);
+alert("I would like to ask you a few questions. Just yes or no answers please");
+
+ 
+let Question01 = "Is my name John? "
+let nameGuess = prompt(Question01).toLowerCase();
+counter += answerQuestions_yes_or_no(Question01, nameGuess, 'y');
+console.log("counter1 ", counter)
+
+
+let Question02 = "Was i born in Washington State?"
+let ageGuess = prompt(Question02).toLowerCase();
+counter += answerQuestions_yes_or_no(Question02, ageGuess, 'y');
+console.log("counter2 ", counter)
+
+
+let Question03 = "Do i jump out of perfectly good airplanes ?"
+let majorGuess = prompt(Question03).toLowerCase();
+counter += answerQuestions_yes_or_no(Question03, majorGuess, 'y');
+console.log("counter3 ", counter)
+
+
+let Question04 = "Do i like building things?"
+let statusGuess = prompt(Question04).toLowerCase();
+counter += answerQuestions_yes_or_no(Question04, statusGuess, 'y');
+console.log("counter4 ", counter)
+
+
+let Question05 = "Is my goal to master PYTHON?"
+let confermGuess = prompt(Question05).toLowerCase();
+counter += answerQuestions_yes_or_no(Question05, confermGuess, 'y');
+console.log("counter5= ", counter)
+
+alert(`Looks like you read my bio, your a machine ${UserInput}`);
+
+
+
+
+
